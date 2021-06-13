1b839a6fa00b91a973c9b56ba1698d570ea1ff3e
Yitzy Bratt <yitzybratt32@gmail.com>
2021-06-12 22:44:01 -0400

    axios request from state backend

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-/* global Datamap */
+/* global Datamap axios */
+axios.get("http://localhost:3000/api/states").then(response => {
+  console.log(response.data);
+});
 var map = new Datamap({
   element: document.getElementById('container'),
   scope: 'usa'
